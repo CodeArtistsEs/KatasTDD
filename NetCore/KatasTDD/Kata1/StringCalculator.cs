e916d74a5d56ded14a89cbbf33ba8a31d4bbd10f
Ximo <ximo.coder@gmail.com>
2018-01-29 20:42:51 +0100

    Added StringCalculator TDD

---
+++ NetCore/KatasTDD/Kata1/StringCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace KatasTDD.Kata1
+{
+    public class StringCalculator
+    {
+        public int Add(string parameters)
+        {
+            if (string.IsNullOrEmpty(parameters.Trim()) || (parameters.Length == 0))
+                return 0;
+
+            int sumOfNumbers = 0;
+            var stringArray = parameters.Trim().Split(Environment.NewLine.ToCharArray());
+            foreach (var str in stringArray)
+            {
+                sumOfNumbers += str.Split(',').Sum(x => int.Parse(x));
+            }
+
+            return sumOfNumbers;
+        }
+    }
+}
---
+++ NetCore/KatasTDD/Kata1/StringCalculatorTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KatasTDD.Kata1
+{
+    /// <summary>
+    /// http://osherove.com/tdd-kata-1/
+    /// </summary>
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+
+        }
+
+        [TestMethod]
+        public void EmptyStringShouldBeZero()
+        {
+            var stringCalculator = new StringCalculator();
+            var res = stringCalculator.Add("");
+
+            Assert.AreEqual(res, 0);
+        }
+
+        [TestMethod]
+        public void WhiteSpaceShouldBeZero()
+        {
+            var stringCalculator = new StringCalculator();
+            var res = stringCalculator.Add(" ");
+
+            Assert.AreEqual(res, 0);
+        }
+
+        [TestMethod]
+        public void OneShouldBeOne()
+        {
+            var stringCalculator = new StringCalculator();
+            var res = stringCalculator.Add("1");
+
+            Assert.AreEqual(res, 1);
+        }
+
+        [TestMethod]
+        public void NumberCommaNumberShouldBeSum()
+        {
+            var stringCalculator = new StringCalculator();
+            var res = stringCalculator.Add("1,2");
+
+            Assert.AreEqual(res, 3);
+        }
+
+        [TestMethod]
+        public void SpacesShouldBeIgnored()
+        {
+            var stringCalculator = new StringCalculator();
+            var res = stringCalculator.Add("  1");
+
+            Assert.AreEqual(res, 1);
+        }
+
+        [TestMethod]
+        public void NewLineShouldSeparateNumbers()
+        {
+            var stringCalculator = new StringCalculator();
+            var res = stringCalculator.Add("\n 1 \n  2 , 3");
+
+            Assert.AreEqual(res, 6);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void FailOnNonNumericCharacter()
+        {
+            var stringCalculator = new StringCalculator();
+            var res = stringCalculator.Add("1\n $%& 2 , 3");
+        }
+
+    }
+
+}
